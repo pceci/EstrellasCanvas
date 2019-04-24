ef549a765987d92e40c6bbbd8804bb5c775081d5
CeciP <CeciP@sancristobal.com.ar>
2019-04-24 10:42:08 -0300

    [DONE] Limpieza código

--- code.js
+++ code.js
@@ -4,20 +4,38 @@ var MaxX = 700;
 var MaxY = 700;
 var MinX = 0;
 var MinY = 0;
-//var pxReferencia = 0.00001;
-//var radReferencia = 1 + (((Math.PI * 2) * (1)) / 86400);
-// Declinación 0 a 90 grados
-//var decReferencia = 1 + ((500 * 0.00002) * 90);
-var ctx = null;
+
+var ctxSur = null;
 var ctxNorte = null;
 var listaEstrellas = null;
 
 jQuery(document).ready(
 
 function () {
+    MostrarPlanoCartesiano();
 
-    var c = document.getElementById("divCanvasSur");
-    ctx = c.getContext("2d");
+    CargarEstrellas();
+
+});
+function MostrarPlanoCartesiano() {
+
+    // Polo Sur 
+    var cSur = document.getElementById("divCanvasSur");
+    ctxSur = cSur.getContext("2d");
+
+    // EJE DE ABSCISAS 
+    ctxSur.moveTo(0, centroY);
+    ctxSur.lineTo(MaxX, centroY);
+
+    //EJE DE ORDENADAS
+    ctxSur.moveTo(centroX, 0);
+    ctxSur.lineTo(centroX, MaxY);
+
+    ctxSur.strokeStyle = 'blue';
+    ctxSur.lineWidth = 0.4;
+    ctxSur.stroke();
+    // Fin Polo Sur 
+
     // Polo Norte 
     var cNorte = document.getElementById("divCanvasNorte");
     ctxNorte = cNorte.getContext("2d");
@@ -31,32 +49,23 @@ function () {
     ctxNorte.lineTo(centroX, MaxY);
 
     ctxNorte.strokeStyle = 'blue';
+    ctxNorte.lineWidth = 0.4;
     ctxNorte.stroke();
-
-    //ctxNorte.strokeStyle = '#5A3F3F';
-
-
     // Fin Polo Norte
-    // EJE DE ABSCISAS 
-    ctx.moveTo(0, centroY);
-    ctx.lineTo(MaxX, centroY);
+}
+function CargarEstrellas() {
+    listaEstrellas = eval('(' + $('#hiddenListaEstrellas').val() + ')');
+    for (var i = 0; i < listaEstrellas.length; i++) {
+        if (listaEstrellas[i].dec <= 0) {
+            var vectEstrella = obtenerVectorComponente(ObtenerMagnitud((listaEstrellas[i].dec * -1)), ((Math.PI * 2) * listaEstrellas[i].ra) / 24);
+            ctxSur.fillRect(vectEstrella.x + centroX, centroY - vectEstrella.y,2, 2);
+        } else {
+            var vectEstrella = obtenerVectorComponente(ObtenerMagnitud(listaEstrellas[i].dec), ((Math.PI * 2) * listaEstrellas[i].ra) / 24);
+            ctxNorte.fillRect(vectEstrella.x + centroX, centroY - vectEstrella.y, 2, 2);
 
-    //EJE DE ORDENADAS
-    ctx.moveTo(centroX, 0);
-    ctx.lineTo(centroX, MaxY);
-
-    ctx.strokeStyle = 'blue';
-    ctx.stroke();
-
-    //ctx.strokeStyle = '#5A3F3F';
-
-
-    //ctx.strokeStyle = 'red';
-
-    ObtenerEstrellas();
-
-});
-
+        }
+    }
+}
 function ObtenerMagnitud(pValor) {
     var resultado = 0;
     if (pValor == 90) {
@@ -70,35 +79,12 @@ function ObtenerMagnitud(pValor) {
 }
 
 function obtenerVectorComponente(pMagnitud, pAnguloRadianes) {
-
-
     var obj = new VectorComponente();
     obj.x = pMagnitud * Math.cos(pAnguloRadianes);
     obj.y = pMagnitud * Math.sin(pAnguloRadianes);
-
-
     return obj;
 }
 function VectorComponente() {
-
-
     this.x = 0;
-
-
     this.y = 0;
-}
-
-
-function ObtenerEstrellas() {
-    listaEstrellas = eval('(' + $('#hiddenListaEstrellas').val() + ')');
-    for (var i = 0; i < listaEstrellas.length; i++) {
-        if (listaEstrellas[i].dec <= 0) {
-            var vectEstrella = obtenerVectorComponente(ObtenerMagnitud((listaEstrellas[i].dec * -1)), ((Math.PI * 2) * listaEstrellas[i].ra) / 24);
-            ctx.fillRect(vectEstrella.x + centroX, centroY - vectEstrella.y,2, 2);
-        } else {
-            var vectEstrella = obtenerVectorComponente(ObtenerMagnitud(listaEstrellas[i].dec), ((Math.PI * 2) * listaEstrellas[i].ra) / 24);
-            ctxNorte.fillRect(vectEstrella.x + centroX, centroY - vectEstrella.y, 2, 2);
-
-        }
-    }
 }
